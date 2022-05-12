89264efbf3de853005605862bf127868e1917c6f
clamp <clamp1981@gmail.com>
2022-05-12 22:25:46 +0900

    형변환 주석 추가

--- study/tutorials/kotlinbasics/TypeCast.kt
+++ study/tutorials/kotlinbasics/TypeCast.kt
@@ -49,11 +49,12 @@ fun main(){
         println("obj1 is String , Length ${obj1.length}")
     }
 
-    //안전 하지 않은 형변환
+    //안전 하지 않은 형변환 ( as 키워드  )
     //obj1이 문자열이 아닌 경우 문제가 생김 ( error 발생 )
     val strig1 : String = obj1 as String
     println(strig1.length)
 
+    //안전 한 형변환 ( as? 키워드  )
     val obj2 :Any = 1337
     val strig2 : String? = obj2 as? String
     println(strig2) //문자열이 아니면 null
